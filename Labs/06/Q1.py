1f4e7b18cd58e7108cdae372dffa5775ef231a58
syedhuzaifa2005 <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-09-29 19:59:37 +0500

    Update Q1.py

--- Labs/06/Q1.py
+++ Labs/06/Q1.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 movies = {
-  'Title': [movie 1, movie 2, movie 3, movie 4, movie 5, movie 6],
+  'Title': ['movie 1', 'movie 2', 'movie 3', 'movie 4', 'movie 5', 'movie 6'],
   'Revenue': [1.8, 2.7, 4.9, 0.8, 3.2, 2.2],
-  'Budget': [1, 1.3, 2.1, 0.5, 1.2, 0.8]
+  'Budget': [1, 1.3, 0.7, 0.5, 0.9, 0.8]
 }
 
 df = pd.DataFrame(movies)
-filtered_movies = df[(df['Revenue'] > 2) and (df['Budget'] < 1)]
+filtered_movies = df[(df['Revenue'] > 2) & (df['Budget'] < 1)]
 print(filtered_movies)
