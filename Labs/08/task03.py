414c04c678fffbe82414964759de7d67f9d3a309
Syed Huzaifa Ali <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-10-09 09:15:02 +0500

    Update task03.py

--- Labs/08/task03.py
+++ Labs/08/task03.py
@@ -1,17 +1,6 @@
-url = input("Enter URL: ")
+import numpy as np
 
-def process_url(url):
-    if url.startswith("http://www."):
-        url = url[len("http://www."):]
-    elif url.startswith("https://www."):
-        url = url[len("https://www."):]
-    else:
-        print("Enter a valid URL starting with 'http://www.' or 'https://www.'")
-        exit()
-
-    if url.endswith(".com"):
-        print(url)
-    else:
-        print(url + ".com")
-
-process_url(url)
+arr = np.linspace(0, 16, 9)
+print(arr)
+matrix = arr.reshape(3, 3)
+print(matrix)
