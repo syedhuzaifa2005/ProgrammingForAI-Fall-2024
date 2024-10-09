851d082a6520fbb4b998ea6307334a7e2978c4fa
Syed Huzaifa Ali <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-10-09 09:14:47 +0500

    Update task02.py

--- Labs/08/task02.py
+++ Labs/08/task02.py
@@ -1,18 +1,6 @@
-def check_last_letter(input_string):
-    vowels = 'aeiouAEIOU'
-    
-    input_string = input_string.strip()
+import numpy as np
 
-    if not input_string:
-        return "The input string is empty."
-    
-    last_char = input_string[-1]
-    
-    if last_char in vowels:
-        return f"The last letter '{last_char}' is a vowel."
-    else:
-        return f"The last letter '{last_char}' is a consonant."
-
-user_input = input("Enter a string: ")
-result = check_last_letter(user_input)
-print(result)
+arr = np.arange(1, 19, 2)
+print(arr)
+matrix = arr.reshape(3, 3)
+print(matrix)
