1193c2ad2c28a6095e6036f63ea1f2628c92c1a2
Syed Huzaifa Ali <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-10-14 18:58:17 +0500

    Update task04.py

--- Labs/08/task04.py
+++ Labs/08/task04.py
@@ -1,1 +1,13 @@
+import numpy as np
 
+dtype = [('name', 'U10'), ('height', 'f4'), ('class', 'i4')]
+
+students = np.array([
+    ('Sabeeh', 5.5, 10),
+    ('Haris', 5.9, 9),
+    ('Rayyan', 5.6, 10),
+    ('Huzaifa', 6.2, 9)
+], dtype=dtype)
+
+sorted_students = np.sort(students, order=['class', 'height'])
+print(sorted_students)
