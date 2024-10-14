d981c407daab688ace846c2e6896f318355589b2
Syed Huzaifa Ali <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-10-14 19:01:48 +0500

    Update task05.py

--- Labs/08/task05.py
+++ Labs/08/task05.py
@@ -1,1 +1,18 @@
+import numpy as np
 
+choices = [2, 5, 9, 10]
+matrix_a = np.random.choice(choices, size=(4, 4))
+
+identity_matrix = np.eye(4)
+result_matrix = np.dot(matrix_a, identity_matrix)
+
+odd_matrix = np.array([[1, 3, 5, 7],
+                       [9, 11, 13, 15],
+                       [17, 19, 21, 23],
+                       [25, 27, 29, 31]])
+
+final_matrix = result_matrix + odd_matrix
+
+print("Matrix A:\n", matrix_a)
+print("\nResult after multiplication with identity matrix:\n", result_matrix)
+print("\nFinal matrix after addition:\n", final_matrix)
