e5cf50a27698d744442765355277b5206e7b7991
Syed Huzaifa Ali <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-10-29 17:08:00 +0500

    Delete Assignments/task08.py

--- Assignments/task08.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-A = np.array([[2, 3, 1], [4, 1, -2], [-1, 2, 3]])
-b = np.array([1, -2, 3])
-
-solution = np.round(np.linalg.solve(A, b), 3)
-print("Solution (x, y, z):", solution)
