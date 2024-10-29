87eb3a4b067d3f558375345b8362e57a44499400
Syed Huzaifa Ali <142867893+syedhuzaifa2005@users.noreply.github.com>
2024-10-29 17:07:50 +0500

    Delete Assignments/task07.py

--- Assignments/task07.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-array = np.random.randint(1, 600, 50)
-max_index = np.argmax(array)
-min_index = np.argmin(array)
-
-print("Array:", array)
-print("Index of Maximum Value:", max_index)
-print("Index of Minimum Value:", min_index)
